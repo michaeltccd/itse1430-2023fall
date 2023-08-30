2477ed5edfbe07bfe51012d2b369307b71d488f7
michael.taylor <michael.taylor@my.tccd.edu>
2023-08-29 21:16:52 -0500

    Started  movie library

---
+++ classwork/MovieLibrary/MovieLibrary.ConsoleHost/Program.cs
@@ -0,0 +1,41 @@
+﻿// Movie data:
+//  Title, genre, description, mpaa rating
+//  Length, release year, budget
+//  IsBlackAndWhite
+// Operations: Add, edit, view, delete
+
+string title = "", description = "";
+//title = "";
+string genre = "", rating = "";
+
+int length = 0, releaseYear = 1900;
+
+decimal budget = 125.45M;
+bool isBlackAndWhite = false;
+
+//Get the title
+Console.WriteLine("Enter a title: ");
+title = Console.ReadLine();
+
+Console.WriteLine("Enter a description: ");
+description = Console.ReadLine();
+
+//TODO: Fix length
+Console.WriteLine("Enter the run length (in mins): ");
+string lengthString = Console.ReadLine();
+
+Console.WriteLine(title);
+Console.WriteLine(description);
+Console.WriteLine(lengthString);
+
+//double someFloatingValue = 3.14159;
+//char letterGrade = 'A';
+
+//{
+//    int hours = 5;
+//    //int title = 54;
+//    title = "Jaws";
+
+//    Console.WriteLine(title);
+//    Console.WriteLine(length);
+//}
