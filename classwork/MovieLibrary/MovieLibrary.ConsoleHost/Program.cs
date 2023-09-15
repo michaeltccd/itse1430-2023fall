bb09906ce7a068ed8348dd786eee5a47b47b7ecc
michael.taylor <michael.taylor@my.tccd.edu>
2023-09-14 21:34:46 -0500

    Added business library project. Created Movie class and migrated data to it from program.

--- classwork/MovieLibrary/MovieLibrary.ConsoleHost/Program.cs
+++ classwork/MovieLibrary/MovieLibrary.ConsoleHost/Program.cs
@@ -1,45 +1,33 @@
-﻿// Movie data:
-//  Title, genre, description, mpaa rating
-//  Length, release year, budget
-//  IsBlackAndWhite
-// Operations: Add, edit, view, delete
+﻿/*
+ * ITSE 1430
+ * Fall 2023
+ * 
+ * Sample movie library
+ */
+using MovieLibrary;
 
-//Movie details
-string title = "", description = "";
-string genre = "", rating = "";
-
-Int32 len; // int len;
-int length = 0, releaseYear = 1900;
-bool isBlackAndWhite = false;
-
-//Not using
-//decimal budget = 125.45M;
+//TODO: Remove this
+Movie movie = new Movie();
 
 //Entry point
 var done = false;
 do
 {
-    //int command = DisplayMenu();
-    //if (command == 1)
-    //    AddMovie();
-    //else if (command == 2)
-    //    EditMovie();
-    //else if (command == 3)
-    //    DeleteMovie();
-    //else if (command == 4)
-    //    ViewMovie();
-    //else if (command == 0)
-    //    done = true;
     switch (DisplayMenu())
     {
-        case 1: AddMovie(); break;
-        case 2: EditMovie(); break;
-        case 3: DeleteMovie(); break;
-        case 4: ViewMovie(); break;
-        case 0:
+        case MenuCommand.Add: movie = AddMovie(); break;
+        case MenuCommand.Edit: EditMovie(); break;
+        case MenuCommand.Delete:
         {
-            done = true; 
-            //Do more stuff here
+            //TODO: Clean this up
+            if (DeleteMovie(movie))
+                movie = new Movie();
+            break;
+        };
+        case MenuCommand.View: ViewMovie(movie); break;
+        case MenuCommand.Quit:
+        {
+            done = true;             
             break;
         };
 
@@ -49,7 +37,7 @@ do
 
 /// Functions
 
-int DisplayMenu () 
+MenuCommand DisplayMenu () 
 {
     Console.WriteLine("-----------");
     Console.WriteLine("A)dd Movie");
@@ -60,55 +48,34 @@ int DisplayMenu ()
 
     do
     {
-        //var input = Console.ReadLine();
-        //if (input == "A" || input == "a")
-        //    return 1;
-        //else if (input == "E" || input == "e")
-        //    return 2;
-        //else if (input == "D" || input == "d")
-        //    return 3;
-        //else if (input == "V" || input == "v")
-        //    return 4;
-        //else if (input == "Q" || input == "q")
-        //    return 0;
         switch(Console.ReadKey(true).Key)
         {
-            //case "A": //return 1;
-            //case "a": return 1;
-            case ConsoleKey.A: return 1;
-
-            //case "E": //return 2;
-            //case "e": return 2;
-            case ConsoleKey.E: return 2;
-
-            //case "D": //return 3;
-            //case "d": return 3;
-            case ConsoleKey.D: return 3;
-
-            //case "V": //return 4;
-            //case "v": return 4;
-            case ConsoleKey.V: return 4;
-
-            //case "Q": //return 0;
-            //case "q": return 0;
-            case ConsoleKey.Q: return 0;
+            case ConsoleKey.A: return MenuCommand.Add;
+            case ConsoleKey.E: return MenuCommand.Edit;
+            case ConsoleKey.D: return MenuCommand.Delete;
+            case ConsoleKey.V: return MenuCommand.View;
+            case ConsoleKey.Q: return MenuCommand.Quit;
         };
     } while (true);
 }
 
 //Get a new movie
-void AddMovie ()
-{    
-    title = ReadString("Enter a title: ", true);    
-    description = ReadString("Enter a description: ", false);
-    
-    length = ReadInt("Enter the run length (in mins): ", 0);
-    releaseYear = ReadInt("Enter the release year: ", 1900);
+Movie AddMovie ()
+{
+    var movie = new Movie();
 
-    genre = ReadString("Enter a genre: ", false);
-    rating = ReadRating("Enter a rating: ");
+    movie.title = ReadString("Enter a title: ", true);
+    movie.description = ReadString("Enter a description: ", false);
 
-    isBlackAndWhite = ReadBoolean("Black and White (Y/N)?");
+    movie.length = ReadInt("Enter the run length (in mins): ", 0);
+    movie.releaseYear = ReadInt("Enter the release year: ", 1900);
+
+    movie.genre = ReadString("Enter a genre: ", false);
+    movie.rating = ReadRating("Enter a rating: ");
+
+    movie.isBlackAndWhite = ReadBoolean("Black and White (Y/N)?");
+
+    return movie;
 }
 
 void EditMovie()
@@ -116,89 +83,44 @@ void EditMovie()
     Console.WriteLine("Not implemented yet");
 }
 
-void DeleteMovie()
+bool DeleteMovie ( Movie movie )
 {
-    if (String.IsNullOrEmpty(title))
-        return;
+    if (String.IsNullOrEmpty(movie.title))
+        return false;
 
-    if (!Confirm($"Are you sure you want to delete the movie '{title}' (Y/N)?"))
-        return;
+    if (!Confirm($"Are you sure you want to delete the movie '{movie.title}' (Y/N)?"))
+        return false;
 
-    title = "";
+    //TODO: Delete movie
+    //title = "";
+    return true;
 }
 
 //Display the movie details
-void ViewMovie ()
+void ViewMovie ( Movie movie )
 {
-    // Length of a string
-    int len = title.Length;
-
-    if (String.IsNullOrEmpty(title))
+    if (String.IsNullOrEmpty(movie.title))
     {
         Console.WriteLine("No movies available");
         return;
     };
 
-    Console.WriteLine();
-    //Console.WriteLine("--------------");
+    Console.WriteLine();    
     Console.WriteLine("".PadLeft(15, '-'));
     
-    //Console.WriteLine("\n--------------");
+    Console.WriteLine(movie.title);
 
-    Console.WriteLine(title);
-
-    //String formatting
-    //Run Length: ## mins
-    //Console.WriteLine("Run Length: " + length + " mins");
-    // 1. Concat or Join
-    //Console.WriteLine("Run Length: " + length.ToString() + " mins");
-    //var message = String.Concat("Run Length: ", length.ToString(), " mins");
-    //Console.WriteLine(message);
-    //var message2 = String.Join(" ", "Run Length:", length, "mins");
-    // 2. String.Format
-    //string message = String.Format("Run Length: {0} mins", length);
-    //Console.WriteLine(message);
-    //decimal price = 45.45252;
-    //String.Format("{0:C}", price);  //$45.45
-    //Console.WriteLine("Run Length: {0} mins", length);    
-    // 3. String interpolation
-    string message = $"Run Length: {length} mins";
+    string message = $"Run Length: {movie.length} mins";
     Console.WriteLine(message);
+        
+    Console.WriteLine($"Released {movie.releaseYear}");
+    Console.WriteLine(movie.genre);
+    Console.WriteLine($"MPAA Rating: {movie.rating}");
 
-    //Released yyyy
-    //Console.WriteLine("Released " + releaseYear);
-    Console.WriteLine($"Released {releaseYear}");
-
-    Console.WriteLine(genre);
-
-    //MPAA Rating: 
-    Console.WriteLine($"MPAA Rating: {rating}");
-
-    //Black and White? 
-    // Conditional: Eb ? Et : Ef
-    //string format = "Color";
-    //if (isBlackAndWhite)
-    //    format = "Black and White";
-
-    //V2
-    string format = isBlackAndWhite ? "Black and White" : "Color";    
+    string format = movie.isBlackAndWhite ? "Black and White" : "Color";    
     Console.WriteLine("Format: ".PadLeft(10) + format);
 
-    //V3
-    //Console.WriteLine("Format: " + (isBlackAndWhite ? "Black and White" : "Color"));
-
-    Console.WriteLine(description);
-
-    //More string functions
-    //string path1 = @"C:\temp";
-    //string path2 = @"Windows";
-    //if (!path1.EndsWith("\\"))
-    //    path1 = path1 + "\\";
-    //if (path2.StartsWith("\\"))
-    //    ///path2 = path2.Substring(1);
-    //    path2 = path2.Trim('\\');
-    //string path3 = path1 + "\\" + path2;
-
+    Console.WriteLine(movie.description);
 }
 
 bool Confirm ( string message )
@@ -253,13 +175,8 @@ int ReadInt ( string message, int minimumValue )
     {
         string value = Console.ReadLine();
 
-        //NOT SAFE
-        //int result = Int32.Parse(value);
-        //int result;
-        //if (Int32.TryParse(value, out result))
         if (Int32.TryParse(value, out var result))
-        //if (int.TryParse(value, out var result))
-                if (result >= minimumValue)
+            if (result >= minimumValue)
                 return result;
 
         Console.WriteLine("Value must be at least " + minimumValue);
@@ -273,15 +190,6 @@ string ReadRating ( string message )
     do
     {
         string value = Console.ReadLine();
-        //if (value == "PG")
-        // 1. String compare 1 -> Don't use
-        //if (value.ToUpper() == "PG")
-        //value = value.ToLower();
-        //if (value == "pg")
-        // 2. String compare 2 -> 
-        //if (String.Compare(value, "PG", true) == 0) // String.Compare(value, "PG", StringComparison.CurrentCultureIgnoreCase)
-        //if (value.CompareTo("PG") == 0)
-        // 3. String Equals -> PREFERRED
         if (String.Equals(value, "PG", StringComparison.CurrentCultureIgnoreCase))
             return "PG";
         else if (String.Equals(value, "G", StringComparison.CurrentCultureIgnoreCase))
@@ -290,8 +198,6 @@ string ReadRating ( string message )
             return "PG-13";
         else if (String.Equals(value, "R", StringComparison.CurrentCultureIgnoreCase))
             return "R";
-        //else if (value == "") // else if (value == String.Empty
-        //else if (value == "" || value == null) //Don't do this
         else if (String.IsNullOrEmpty(value))
             return "";
 
@@ -309,17 +215,19 @@ string ReadString ( string message, bool isRequired )
 
         if (!isRequired || !String.IsNullOrEmpty(value))
             return value;
-        //if (!isRequired)
-        //    return value;
-
-        ////Is it empty?
-        //if (value != "")
-        //    return value;
 
         Console.WriteLine("Value is required");
     } while (true);    
 }
 
+enum MenuCommand
+{
+    Add = 1,
+    Edit,
+    Delete,
+    View,
+    Quit = 0
+}
 #region Demo Code
 
 //void Demo ()
---
+++ classwork/MovieLibrary/MovieLibrary/Movie.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * ITSE 1430
+ * Fall 2023
+ */
+namespace MovieLibrary
+{
+    /// <summary>Represents a movie.</summary>
+    /// <remarks>
+    /// Paragraphs of descriptions.
+    /// </remarks>
+    public class Movie
+    {
+        //Fields - data
+        /// <summary>Title of movie.</summary>
+        public string title = "";
+        public string description = "";
+        public string genre = "";
+        public string rating = "";
+
+        public int length;
+        public int releaseYear = 1900;
+        public bool isBlackAndWhite;
+    }
+}
